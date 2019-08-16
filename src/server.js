38a6bc44738c0817856f98c5c283efe01ccfc3c7
cchampou <clement@champouillon.com>
2019-08-16 18:53:55 +0200

    Fixing call order

--- src/server.js
+++ src/server.js
@@ -10,8 +10,8 @@ const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 const server = express();
 server
   .disable('x-powered-by')
-  .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
   .use(compression())
+  .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
   .get('/*', (req, res) => {
     const context = {};
     const markup = renderToString(
