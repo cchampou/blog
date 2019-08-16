7095c0325341a82cfd1f719bafc4acd7559c6855
cchampou <clement@champouillon.com>
2019-08-16 08:31:27 +0200

    Fixing styles SSR

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { injectGlobal } from 'emotion';
+import { Global, css } from '@emotion/core';
 
 import Header from './header/Header';
 import Home from './Home';
 import About from './About';
 
-injectGlobal`
+const globalStyles = css`
   body, h1 {
     font-family: sans-serif;
     font-weight: lighter;
@@ -18,12 +18,12 @@ injectGlobal`
     text-decoration: none;
     font-weight: lighter;
   }
-  
 `;
 
 
 const App = () => (
   <>
+    <Global styles={globalStyles}/>
     <Header/>
     <Switch>
       <Route exact path="/" component={Home} />
